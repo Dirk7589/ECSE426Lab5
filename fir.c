4ea5618d9fb3c23d9962554ccef5ffa43866581f
unknown <dduboi4@156TR4160-17.campus.MCGILL.CA>
2013-03-15 14:05:50 -0400

    Initial commit:

---
+++ fir.c
@@ -0,0 +1,10 @@
+ /**
+*@file fir.c
+*@author Dirk Dubois, Alain Slak
+*@date February 21th, 2013
+*@brief 
+*
+*/
+
+/*Includes*/
+#include <stdint.h>
---
+++ fir.h
@@ -0,0 +1,13 @@
+ /**
+*@file fir.h
+*@author Dirk Dubois, Alain Slak
+*@date February 21th, 2013
+*@brief 
+*
+*/
+
+#ifndef __FIR_H
+#define __FIR_H
+
+
+#endif
