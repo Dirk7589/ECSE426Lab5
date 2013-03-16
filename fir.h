f6a68a05f4c9bbd17b30ca968eca8b58dc949f56
Dirk7589 <dirk.dubois@mail.mcgill.ca>
2013-03-16 14:26:38 -0400

    Added to struct

--- fir.h
+++ fir.h
@@ -10,30 +10,34 @@
 #define __FIR_H
 
 /*Defines*/
-#define NUMBER_OF_SAMPLES 10 /**<The number of samples pass to the filter
+#define NUMBER_OF_SAMPLES 19 /**<The number of samples pass to the filter*/
+#define SCALE 10000
 
 /**@struct FIR_DATA
 * A structure that holds all necessary values for the FIR filter
 */
 
 typedef struct{
-	int16_t coefficients[19];
+	int16_t coeffFixed[19];
 	int16_t values[NUMBER_OF_SAMPLES];
+	int16_t* lowestIndex;
 	int16_t result;
-}FIR_DATA
+	uint8_t count;
+}FIR_DATA;
 
 /**
 *@brief A function that acts as a FIR filter
-*
-*@retval None
+*@param[in] value The newest value to the filter
+*@param[in] data The data structure for the filter
+*@retval The flag indicating a new output is ready, 1 indicates a sample is ready
 */
-void fir(void);
+uint8_t fir(float32_t value, FIR_DATA* data);
 
 /**
 *@brief A function that initializes the FIR filter
-*
+*@param[in] data The data structure for the filter
 *@retval None
 */
-void firInit(void);
+void firInit(FIR_DATA* data);
 
 #endif
